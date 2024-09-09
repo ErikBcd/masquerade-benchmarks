25e700072fda9f7daf718029d5701b31c560a726
ErikBcd <Erik.Bach@gmx.de>
2024-09-09 20:24:03 +0200

    Added tests where server and client run on different systems

--- test-images/build_all.sh
+++ test-images/build_all.sh
@@ -6,5 +6,8 @@ docker buildx build --tag iperf3_client ./iperf3-docker/
 echo "Building Masquerade Client image!"
 docker buildx build --tag masq_iperf3_client ./masquerade-client/ --network=host
 
+echo "Building Masquerade Client External Server Image"
+docker buildx build --tag masq_iperf3_client_external ./masquerade-client-external/ --network=host 
+
 echo "Building Masquerade Server image!"
 docker build -t masq_server ./masquerade-server-docker/ --network=host
