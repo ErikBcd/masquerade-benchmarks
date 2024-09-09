5fe8c0321de91fcf7d1aeef63e13357ea8780f8a
ErikBcd <Erik.Bach@gmx.de>
2024-09-09 20:25:47 +0200

    Modified start script for split system tests, wait 5s after each test so the server has time to restart

--- data-gathering/split-client-server/client/run_tests.sh
+++ data-gathering/split-client-server/client/run_tests.sh
@@ -9,17 +9,28 @@ BITRATE_START=50
 BITRATE_STEP=50
 BITRATE_END=1000
 
-SERVER_IP="192.168.0.45"
+SERVER_IP="10.8.0.1"
 
 docker compose down --remove-orphans
 
 let "num_tests=($BITRATE_END / $BITRATE_STEP) * $TESTS * 4"
-let "seconds=$num_tests * ($TEST_TIME + 5)"
+let "seconds=$num_tests * ($TEST_TIME + 10)"
 let "in_hours=($seconds / 60) / 60"
 
 echo "Running $num_tests tests, will be done in ${in_hours}h"
 
 # Run tests with different bitrates in succession
+
+# test TCP and UDP
+echo "TCP TEST"
+
+docker compose run -e BITRATE="500" -e TESTTIME="60" -e PARALLEL="1" -e TESTCASE="INITIAL_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT=1 iperf3
+docker-compose down --remove-orphans
+
+sleep 5
+echo "UDP TEST"
+docker compose run -e BITRATE="50" -e TESTTIME="10" -e PARALLEL="1" -e TESTCASE="INITIAL_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT=2 iperf3
+docker-compose down --remove-orphans
 
 # TCP UPLOAD
 for x in $(eval echo "{$START..$TESTS}")
@@ -29,6 +40,7 @@ do
         echo "Testing $i M ($x / $TESTS)"
         docker compose run -e BITRATE="$i" -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="$x" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT=1 iperf3
         docker-compose down --remove-orphans
+        sleep 5
     done
     echo "Done ($x / $TESTS) for TCP UPLOAD"
 done
@@ -41,6 +53,7 @@ do
         echo "Testing $i M"
         docker compose run -e BITRATE="$i" -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="$x" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT=2 iperf3
         docker-compose down --remove-orphans
+        sleep 5
     done
     echo "Done ($x / $TESTS) for UDP UPLOAD"
 done
@@ -53,6 +66,7 @@ do
         echo "Testing $i M"
         docker compose run -e BITRATE="$i" -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="$x" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT=3 iperf3
         docker-compose down --remove-orphans
+        sleep 5
     done
     echo "Done ($x / $TESTS) for TCP DOWNLOAD"
 done
@@ -65,6 +79,7 @@ do
         echo "Testing $i M"
         docker compose run -e BITRATE="$i" -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="$x" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT=4 iperf3
         docker-compose down --remove-orphans
+        sleep 5
     done
     echo "Done ($x / $TESTS) for UDP DOWNLOAD"
 done
