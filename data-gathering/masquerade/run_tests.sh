e87dcedccc4ed73c02fee11b58b1c18a0f630792
ErikBcd <Erik.Bach@gmx.de>
2024-09-11 20:28:31 +0200

    Added limit testcases to testrunner

--- data-gathering/masquerade/run_tests.sh
+++ data-gathering/masquerade/run_tests.sh
@@ -19,7 +19,45 @@ let "in_hours=($seconds / 60) / 60"
 
 echo "Running $num_tests tests, will be done in ${in_hours}h"
 
-docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="PACKETLOSS_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT=2 iperf3
+## TESTS WITH tc LIMITATIONS
+# TCP UPLOAD
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="PACKETLOSS_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="TCP-UPLOAD-PACKETLOSS" iperf3
+docker-compose down --remove-orphans
+
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="DELAY_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="TCP-UPLOAD-DELAY" iperf3
+docker-compose down --remove-orphans
+
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="BANDWIDTH_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="TCP-UPLOAD-BANDWIDTH" iperf3
+docker-compose down --remove-orphans
+
+# TCP DOWNLOAD
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="PACKETLOSS_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="TCP-DOWNLOAD-PACKETLOSS" iperf3
+docker-compose down --remove-orphans
+
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="DELAY_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="TCP-DOWNLOAD-DELAY" iperf3
+docker-compose down --remove-orphans
+
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="BANDWIDTH_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="TCP-DOWNLOAD-BANDWIDTH" iperf3
+docker-compose down --remove-orphans
+
+# UDP UPLOAD
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="PACKETLOSS_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="UDP-UPLOAD-PACKETLOSS" iperf3
+docker-compose down --remove-orphans
+
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="DELAY_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="UDP-UPLOAD-DELAY" iperf3
+docker-compose down --remove-orphans
+
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="BANDWIDTH_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="UDP-UPLOAD-BANDWIDTH" iperf3
+docker-compose down --remove-orphans
+
+# UDP DOWNLOAD
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="PACKETLOSS_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="UDP-DOWNLOAD-PACKETLOSS" iperf3
+docker-compose down --remove-orphans
+
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="DELAY_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="UDP-DOWNLOAD-DELAY" iperf3
+docker-compose down --remove-orphans
+
+docker compose run -e BITRATE=50 -e TESTTIME="$TEST_TIME" -e PARALLEL="$PARALLEL" -e TESTCASE="BANDWIDTH_TEST" -e IPERF_SERVER_IP="$SERVER_IP" -e CURRENT="UDP-DOWNLOAD-BANDWIDTH" iperf3
 docker-compose down --remove-orphans
 
 # Run tests with different bitrates in succession
