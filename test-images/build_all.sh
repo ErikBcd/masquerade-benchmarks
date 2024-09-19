401ed1b8530b0cc5732bcfc89a11658b39323f9e
ErikBcd <Erik.Bach@gmx.de>
2024-09-19 21:23:17 +0200

    iperf now supports download network conditions

--- test-images/build_all.sh
+++ test-images/build_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Building iperf3 client image!"
-docker buildx build --tag iperf3_client ./iperf3-docker/
+docker buildx build --tag iperf3_client ./iperf3-docker/ --no-cache
 
 echo "Building Masquerade Client image!"
-docker buildx build --tag masq_iperf3_client ./masquerade-client/ --network=host
+docker buildx build --tag masq_iperf3_client ./masquerade-client/ --network=host --no-cache
 
-echo "Building Masquerade Client External Server Image"
-docker buildx build --tag masq_iperf3_client_external ./masquerade-client-external/ --network=host 
+#echo "Building Masquerade Client External Server Image"
+#docker buildx build --tag masq_iperf3_client_external ./masquerade-client-external/ --network=host 
 
 echo "Building Masquerade Server image!"
-docker build -t masq_server ./masquerade-server-docker/ --network=host
+docker build -t masq_server ./masquerade-server-docker/ --network=host  --no-cache
--- test-images/iperf3-docker/start.sh
+++ test-images/iperf3-docker/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+modprobe ifb
+ip link add ifb0 type ifb
+ip link set dev ifb0 up
+
 # Wait a little till the client is running
 sleep 3s
 
