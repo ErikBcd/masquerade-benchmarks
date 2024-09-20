0746d890093129428b3b45e4d7b555efc0d1f33b
ErikBcd <Erik.Bach@gmx.de>
2024-09-20 03:26:46 +0200

    Updated the plots to be more pretty and easy to read

--- test-analysis/src/two_plots.py
+++ test-analysis/src/two_plots.py
@@ -9,6 +9,15 @@ from testparser import Iperf3DataTCP, Iperf3DataUDP
 import pandas as pd
 import matplotlib.pyplot as plt
 
+wireguard_color = '#e02214'
+wireguard_area_color = '#eba09b'
+masquerade_color = '#1e14e0'
+masquerade_area_color = '#9d9bcf'
+
+fontsize=30
+figuresize=(13,9)
+figuresize_wider=(15,9)
+
 def main():
     masq_dir = path.normpath(
         "../raw-test-results/60s-1000mbits-50mbitInterval-masquerade/"
@@ -153,28 +162,28 @@ def analyze_tcp(tcp_tests_masq: list[Iperf3DataTCP], tcp_tests_wg: list[Iperf3Da
         retrans_upload_wg,
         retrans_upload_masq,
         base_path + "retransmits_vs_bitrate_tcp_upload",
-        "TCP Upload Target Bitrate vs Retransmitted Packets",
+        "TCP Upload Target Bitrate\nvs Retransmitted Packets",
     )
 
     retransmit_plot(
         retrans_dwload_wg,
         retrans_dwload_masq,
         base_path + "retransmits_vs_bitrate_tcp_download",
-        "TCP Download Target Bitrate vs Retransmitted Packets",
+        "TCP Download Target Bitrate\nvs Retransmitted Packets",
     )
 
     target_vs_actual_plot(
         bps_upload_wg,
         bps_upload_masq,
         base_path + "bps_vs_target_tcp_upload",
-        "TCP Upload Target Bitrate vs Measured Bitrate"
+        "TCP Upload Target Bitrate\nvs Measured Bitrate"
     )
 
     target_vs_actual_plot(
         bps_dwload_wg,
         bps_dwload_masq,
         base_path + "bps_vs_target_tcp_download",
-        "TCP Download Target Bitrate vs Measured Bitrate"
+        "TCP Download Target Bitrate\nvs Measured Bitrate"
     )
     
     target_vs_rtt_plot(
@@ -213,6 +222,7 @@ def analyze_udp(udp_tests_masq: list[Iperf3DataUDP], udp_tests_wg: list[Iperf3Da
     masq_bitrate_vs_target_upload = []
     masq_bitrate_vs_target_download = []
     masq_lost_vs_target_upload = []
+    masq_lost_vs_target_download = []
     
     masq_jitter_vs_target_upload = []
     masq_jitter_vs_target_download = []
@@ -220,6 +230,7 @@ def analyze_udp(udp_tests_masq: list[Iperf3DataUDP], udp_tests_wg: list[Iperf3Da
     wg_bitrate_vs_target_upload = []
     wg_bitrate_vs_target_download = []
     wg_lost_vs_target_upload = []
+    wg_lost_vs_target_download = []
     
     wg_jitter_vs_target_upload = []
     wg_jitter_vs_target_download = []
@@ -250,6 +261,11 @@ def analyze_udp(udp_tests_masq: list[Iperf3DataUDP], udp_tests_wg: list[Iperf3Da
                     "target": t.target_bps / 1000000,
                     "jitter": t.jitter_ms,
             })
+            masq_lost_vs_target_download.append({
+                    "target": t.target_bps / 1000000,
+                    "lost": t.lost_packets,
+                    "percentage": t.lost_percent,
+            })
     for t in udp_tests_wg:
         if (t.target_bps / 1000000) >= 950:
             continue
@@ -276,7 +292,19 @@ def analyze_udp(udp_tests_masq: list[Iperf3DataUDP], udp_tests_wg: list[Iperf3Da
                     "target": t.target_bps / 1000000,
                     "jitter": t.jitter_ms,
             })
+            wg_lost_vs_target_download.append({
+                    "target": t.target_bps / 1000000,
+                    "lost": t.lost_packets,
+                    "percentage": t.lost_percent,
+            })
             
+    #lost_packet_plot(
+    #    wg_lost_vs_target_download,
+    #    masq_lost_vs_target_download,
+    #    base_path + "lost_pck_vs_target_bitrate_udp_download",
+    #    "Download"
+    #)
+
     lost_packet_plot(
         wg_lost_vs_target_upload,
         masq_lost_vs_target_upload,
@@ -302,18 +330,20 @@ def analyze_udp(udp_tests_masq: list[Iperf3DataUDP], udp_tests_wg: list[Iperf3Da
         wg_bitrate_vs_target_upload,
         masq_bitrate_vs_target_upload,
         base_path + "bps_vs_target_udp_upload",
-        "UDP Upload Target Bitrate vs Measured Bitrate"
+        "UDP Upload Target Bitrate\nvs Measured Bitrate"
     )
 
     target_vs_actual_plot(
         wg_bitrate_vs_target_download,
         masq_bitrate_vs_target_download,
         base_path + "bps_vs_target_udp_download",
-        "UDP Download Target Bitrate vs Measured Bitrate"
+        "UDP Download Target Bitrate\nvs Measured Bitrate"
     )
 
 def lost_packet_plot(data_wg, data_masq, name, title):
     plt.close()
+    plt.rcParams.update({'font.size': fontsize})
+    plt.rcParams["figure.figsize"] = figuresize
     df_wg = pd.DataFrame(data_wg)
     df_masq = pd.DataFrame(data_masq)
     
@@ -335,45 +365,59 @@ def lost_packet_plot(data_wg, data_masq, name, title):
         std_percentage=('percentage', 'std')
     ).reset_index()
     
-    plt.errorbar(agg_df_wg['target'], agg_df_wg['mean_lost'], yerr=agg_df_wg['std_lost'], fmt='o-', capsize=5, label='WireGuard')
-    plt.errorbar(agg_df_masq['target'], agg_df_masq['mean_lost'], yerr=agg_df_masq['std_lost'], fmt='o-', capsize=5, label='Masquerade')
+    plt.errorbar(agg_df_wg['target'], agg_df_wg['mean_lost'], yerr=agg_df_wg['std_lost'], fmt='o-', capsize=5, label='WireGuard', color=wireguard_color)
+    plt.errorbar(agg_df_masq['target'], agg_df_masq['mean_lost'], yerr=agg_df_masq['std_lost'], fmt='o-', capsize=5, label='Masquerade', color=masquerade_color)
     plt.xlabel('Target Bitrate (Mbps)')
     plt.ylabel('Lost Packets')
     plt.title('Lost Packets (Mean ± Std) vs Target Bitrate | ' + title)
     plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
     plt.savefig(name + "_errorbar.png", dpi=300)
-    plt.savefig(name + "_errorbar.svg")
+    plt.savefig(name + "_errorbar.pdf")
     
     plt.close()
-    
+    plt.rcParams.update({'font.size': fontsize})
+    plt.rcParams["figure.figsize"] = figuresize
     # Shaded area plot for lost packets (min-max range)
-    plt.plot(agg_df_wg['target'], agg_df_wg['mean_lost'], marker='o', label='WireGuard')
-    plt.plot(agg_df_masq['target'], agg_df_masq['mean_lost'], marker='o', label='Masquerade')
-    plt.fill_between(agg_df_wg['target'], agg_df_wg['min_lost'], agg_df_wg['max_lost'], color='gray', alpha=0.3, label='Min-Max Range WireGuard')
-    plt.fill_between(agg_df_masq['target'], agg_df_masq['min_lost'], agg_df_masq['max_lost'], color='gray', alpha=0.3, label='Min-Max Range Masquerade')
+    plt.plot(agg_df_wg['target'], agg_df_wg['mean_lost'], label='WireGuard', color=wireguard_color)
+    plt.plot(agg_df_masq['target'], agg_df_masq['mean_lost'], label='Masquerade', color=masquerade_color)
+    plt.fill_between(agg_df_wg['target'], agg_df_wg['min_lost'], agg_df_wg['max_lost'], color=wireguard_area_color, alpha=0.3, label='Min-Max Range WireGuard')
+    plt.fill_between(agg_df_masq['target'], agg_df_masq['min_lost'], agg_df_masq['max_lost'], color=masquerade_area_color, alpha=0.3, label='Min-Max Range Masquerade')
     plt.xlabel('Target Bitrate (Mbps)')
     plt.ylabel('Lost Packets')
-    plt.title('Lost Packets (Mean with Min-Max Range) vs Target Bitrate | ' + title)
+    plt.title('Lost Packets (Mean with Min-Max Range)\nvs Target Bitrate | ' + title)
+    plt.grid(True)
     plt.legend()
+    plt.tight_layout()
     plt.savefig(name + "_shaded.png", dpi=300)
-    plt.savefig(name + "_shaded.svg")
+    plt.savefig(name + "_shaded.pdf")
     
     
     plt.close()
 
 def jitter_plot(wg_data, masq_data, name, title):
     plt.close()
+    plt.rcParams.update({'font.size': fontsize})
+    plt.rcParams["figure.figsize"] = figuresize
+
     df_wg = pd.DataFrame(wg_data)
     df_wg.sort_values(by=["target"], inplace=True)
     mean_df_wg = df_wg.groupby("target", as_index=False)["jitter"].mean()
+    min_df_wg = df_wg.groupby("target", as_index=False)["jitter"].min()
+    max_df_wg = df_wg.groupby("target", as_index=False)["jitter"].max()
     
     df_masq = pd.DataFrame(masq_data)
     df_masq.sort_values(by=["target"], inplace=True)
     mean_df_masq = df_masq.groupby("target", as_index=False)["jitter"].mean()
+    min_df_masq = df_masq.groupby("target", as_index=False)["jitter"].min()
+    max_df_masq = df_masq.groupby("target", as_index=False)["jitter"].max()
 
-    plt.plot(mean_df_wg["target"], mean_df_wg["jitter"], linestyle="-", label="WireGuard")
-    plt.plot(mean_df_masq["target"], mean_df_masq["jitter"], linestyle="-", label="Masquerade")
-
+    plt.plot(mean_df_wg["target"], mean_df_wg["jitter"], linestyle="-", label="WireGuard", color=wireguard_color)
+    plt.plot(mean_df_masq["target"], mean_df_masq["jitter"], linestyle="-", label="Masquerade", color=masquerade_color)
+    plt.fill_between(min_df_wg['target'], min_df_wg['jitter'], max_df_wg['jitter'], color=wireguard_area_color, alpha=0.3, label='Range WireGuard')
+    plt.fill_between(min_df_masq['target'], min_df_masq['jitter'], max_df_masq['jitter'], color=masquerade_area_color, alpha=0.3, label='Range WireGuard')
+    
     plt.xlabel("Target Bitrate (Mbit/s)")
     plt.ylabel("Jitter (ms)")
     plt.title(title)
@@ -383,18 +427,28 @@ def jitter_plot(wg_data, masq_data, name, title):
     plt.tight_layout()
 
     plt.savefig(name + ".png", dpi=300)
-    plt.savefig(name + ".svg")
+    plt.savefig(name + ".pdf")
     plt.close()
 
 def retransmit_plot(data_wg, data_masq, path, title):
     plt.close()
+    plt.rcParams.update({'font.size': fontsize})
+    plt.rcParams["figure.figsize"] = figuresize
+
     df_wg = pd.DataFrame(data_wg)
     mean_df_wg = df_wg.groupby("target", as_index=False)["retrans"].mean()
+    max_df_wg = df_wg.groupby("target", as_index=False)["retrans"].max()
+    min_df_wg = df_wg.groupby("target", as_index=False)["retrans"].min()
+
     df_masq = pd.DataFrame(data_masq)
     mean_df_masq = df_masq.groupby("target", as_index=False)["retrans"].mean()
+    max_df_masq = df_masq.groupby("target", as_index=False)["retrans"].max()
+    min_df_masq = df_masq.groupby("target", as_index=False)["retrans"].min()
 
-    plt.plot(mean_df_masq["target"], mean_df_masq["retrans"], linestyle="-", label = "Masquerade", color = 'b')
-    plt.plot(mean_df_wg["target"], mean_df_wg["retrans"], linestyle="-", label = "WireGuard", color = 'r')
+    plt.plot(mean_df_masq["target"], mean_df_masq["retrans"], linestyle="-", label = "Masquerade", color = masquerade_color)
+    plt.plot(mean_df_wg["target"], mean_df_wg["retrans"], linestyle="-", label = "WireGuard", color = wireguard_color)
+    plt.fill_between(max_df_wg['target'], min_df_wg['retrans'], max_df_wg['retrans'], color=wireguard_area_color, alpha=0.3, label='Min-Max Range WireGuard')
+    plt.fill_between(max_df_masq['target'], min_df_masq['retrans'], max_df_masq['retrans'], color=masquerade_area_color, alpha=0.3, label='Min-Max Range Masquerade')
     plt.xlabel("Target Bitrate (Mbit/s)")
     plt.ylabel("Retransmissions")
     plt.title(title)
@@ -406,11 +460,14 @@ def retransmit_plot(data_wg, data_masq, path, title):
 
     # Save the plot to a file
     plt.savefig(path + ".png", dpi=300)
-    plt.savefig(path + ".svg")
+    plt.savefig(path + ".pdf")
     plt.close()
 
 def target_vs_actual_plot(data_wg, data_masq, name, title):
     plt.close()
+    plt.rcParams.update({'font.size': fontsize})
+    plt.rcParams["figure.figsize"] = figuresize
+
     df_wg = pd.DataFrame(data_wg)
     df_wg.sort_values(by=["target"], inplace=True)
     mean_df_wg = df_wg.groupby("target", as_index=False)["received"].mean()
@@ -418,8 +475,8 @@ def target_vs_actual_plot(data_wg, data_masq, name, title):
     df_masq.sort_values(by=["target"], inplace=True)
     mean_df_masq = df_masq.groupby("target", as_index=False)["received"].mean()
 
-    plt.plot(mean_df_wg["target"], mean_df_wg["received"], linestyle="-", label = "WireGuard", color = 'r')
-    plt.plot(mean_df_masq["target"], mean_df_masq["received"], linestyle="-", label = "Masquerade", color = 'b')
+    plt.plot(mean_df_wg["target"], mean_df_wg["received"], linestyle="-", label = "WireGuard", color = wireguard_color)
+    plt.plot(mean_df_masq["target"], mean_df_masq["received"], linestyle="-", label = "Masquerade", color = masquerade_color)
     plt.xlabel("Target Bitrate (Mbit/s)")
     plt.ylabel("Actual Bitrate (Mbit/s)")
     plt.title(title)
@@ -430,20 +487,32 @@ def target_vs_actual_plot(data_wg, data_masq, name, title):
     plt.tight_layout()
 
     plt.savefig(name + ".png", dpi=300)
-    plt.savefig(name + ".svg")
+    plt.savefig(name + ".pdf")
     plt.close()
 
 def target_vs_rtt_plot(data_wg, data_masq, name, title):
     plt.close()
+    plt.rcParams.update({'font.size': fontsize})
+    plt.rcParams["figure.figsize"] = figuresize
+
     df_wg = pd.DataFrame(data_wg)
     df_wg.sort_values(by=["target"], inplace=True)
     mean_df_wg = df_wg.groupby("target", as_index=False)["mean_rtt"].mean()
     df_masq = pd.DataFrame(data_masq)
     df_masq.sort_values(by=["target"], inplace=True)
     mean_df_masq = df_masq.groupby("target", as_index=False)["mean_rtt"].mean()
+
+    min_wg = df_wg.groupby("target", as_index=False)["min_rtt"].mean()
+    max_wg = df_wg.groupby("target", as_index=False)["max_rtt"].mean()
+    min_masq = df_masq.groupby("target", as_index=False)["min_rtt"].mean()
+    max_masq = df_masq.groupby("target", as_index=False)["max_rtt"].mean()
     
-    plt.plot(mean_df_wg["target"], mean_df_wg["mean_rtt"], linestyle="-", label = "WireGuard", color = 'r')
-    plt.plot(mean_df_masq["target"], mean_df_masq["mean_rtt"], linestyle="-", label = "Masquerade", color = 'b')
+    plt.plot(mean_df_wg["target"], mean_df_wg["mean_rtt"], linestyle="-", label = "WireGuard", color = wireguard_color)
+    plt.plot(mean_df_masq["target"], mean_df_masq["mean_rtt"], linestyle="-", label = "Masquerade", color = masquerade_color)
+    plt.fill_between(min_wg['target'], min_wg['min_rtt'], max_wg['max_rtt'], color=wireguard_area_color, alpha=0.3, label='Range WireGuard')
+    plt.fill_between(min_masq['target'], min_masq['min_rtt'], max_masq['max_rtt'], color=masquerade_area_color, alpha=0.3, label='Range Masquerade')
+    
+    
     plt.xlabel("Target Bitrate (Mbit/s)")
     plt.ylabel("RTT (ms)")
     plt.title(title)
@@ -454,7 +523,7 @@ def target_vs_rtt_plot(data_wg, data_masq, name, title):
     plt.tight_layout()
 
     plt.savefig(name + ".png", dpi=300)
-    plt.savefig(name + ".svg")
+    plt.savefig(name + ".pdf")
     plt.close()
 
 # Shows how the bps developed over time
@@ -512,6 +581,9 @@ def bps_over_time_plt(
     target):
     
     plt.close()
+    plt.rcParams.update({'font.size': fontsize})
+    plt.rcParams["figure.figsize"] = figuresize
+
     df_wg = pd.DataFrame(data_wg)
     
     #print("wg data: \n" + df_wg)
@@ -522,8 +594,8 @@ def bps_over_time_plt(
     ax = plt.gca()
     ax.set_ylim([0.0, target + 10])
 
-    plt.plot(mean_df_masq["timestamp"], mean_df_masq["bps"], linestyle="-", label = "Masquerade", color = 'b')
-    plt.plot(mean_df_wg["timestamp"], mean_df_wg["bps"], linestyle="-", label = "WireGuard", color = 'r')
+    plt.plot(mean_df_masq["timestamp"], mean_df_masq["bps"], linestyle="-", label = "Masquerade", color = masquerade_color)
+    plt.plot(mean_df_wg["timestamp"], mean_df_wg["bps"], linestyle="-", label = "WireGuard", color = wireguard_color)
     
     plt.legend()
     plt.title(title)
@@ -536,7 +608,7 @@ def bps_over_time_plt(
     # Save the plot to a file
     print("Saving plot as " + path + ".png")
     plt.savefig(path + ".png", dpi=300)
-    plt.savefig(path + ".svg")
+    plt.savefig(path + ".pdf")
     plt.close()
 
 if __name__ == "__main__":
